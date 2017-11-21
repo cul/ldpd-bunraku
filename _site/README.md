e908934926587763f061d50becf1a171692232e2
Marii <mn2802@columbia.edu>
2017-11-21 16:18:57 -0500

    re-add _site

---
+++ _site/README.md
@@ -0,0 +1,20 @@
+# ldpd-bunraku
+Jekyll root and compiled Jekyll `_site` for the Barbara Curtis Adachi Bunraku Collection.
+
+
+Live demo site: [/bunraku-demo](https://cul.github.io/ldpd-bunraku)<br>
+Data processing (iPython) repo: [/bunraku-ipy](https://github.com/mnyrop/bunraku-ipy)
+
+## (online) data set:
+
+- 123 authors
+- 2,107 characters
+- 14,636 images
+- 129 kashira (puppets)
+- 931 performances
+- 184 performers
+- 178 plays
+- 293 productions (festivals)
+- 2,609 scenes (performance-specific)
+- 16,625 "spucks" (scene-puppeteeer-character-kashira combinations)
+- 76 image subject tags
---
+++ _site/spec/lunr_spec.rb
@@ -0,0 +1,36 @@
+$search_tests.each do |search|
+  search_page = search[1]['page']
+  terms = search[1]['terms']
+
+  describe search_page, :type => :feature, :js => true do
+    before(:all) do
+      visit("http://localhost:4000" + $baseurl + "/" + search_page + "/")
+      # puts page.body
+      @search_bar = find(:css, "#search")
+    end
+    it "has a search bar." do
+      expect(@search_bar)
+    end
+    terms.each do |term|
+      context "when searching the term \"" + term + "\"" do
+        before(:all) do
+          @search_bar.set term
+          @result_link = first(".result").first("a")['href']
+        end
+        after(:all) do
+          visit("http://localhost:4000" + $baseurl + "/" + search_page + "/")
+        end
+        it "yields at least 1 result" do
+          expect(@result_link)
+        end
+        it "which sucessfully links to an existing page" do
+          visit(@result_link)
+          expect(status_code == 200)
+        end
+        it "which totally includes " + term + "\"" do
+          expect(have_text(term))
+        end
+      end
+    end
+  end
+end
